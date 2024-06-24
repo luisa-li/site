2c0fc6462d53fa2a824fd9cfc15ce3bb75d37e6c
Luisa <103768169+luisa-li@users.noreply.github.com>
2024-06-23 21:42:10 -0400

    fixed more margin, and also rewrote some stuff

--- portfolio/build.js
+++ portfolio/build.js
@@ -20,20 +20,24 @@ function populateSummary(summary) {
 function populateEducation(education) {
     const educationSection = document.getElementById('education-body');
     const educationContainer = education.map(edu => `
-        <div>
-            <div class="left-right">
-                <h3> ${edu.institution} </h3>
-                <p><i> ${edu.location} </i></p>
+        <div class="portfolio-section">
+            <div>
+                <div class="left-right">
+                    <h3> ${edu.institution} </h3>
+                    <p><i> ${edu.location} </i></p>
+                </div>
+                <div class="left-right">
+                    <h3> ${edu.degree} </h3>
+                    <p><i> ${edu.dates} </i></p>
+                </div>
+                <p><b>Grade: ${edu.grade}</b></p>
             </div>
-            <div class="left-right">
-                <h3> ${edu.degree} </h3>
-                <p><i> ${edu.dates} </i></p>
+            <div>
+                ${edu.honors ? `<p><strong>Honors:</strong> ${edu.honors.join(', ')}</p>` : ''}
+                ${edu.courses && edu.courses.length > 0 ? `
+                    <p><strong>Courses:</strong> ${edu.courses.join(', ')}</p>` : ''}
+                ${edu.description ? `<p>${edu.description}</p>` : ''}
             </div>
-            <p><b>Grade: ${edu.grade}</b></p>
-            ${edu.honors ? `<p><strong>Honors:</strong> ${edu.honors.join(', ')}</p>` : ''}
-            ${edu.courses && edu.courses.length > 0 ? `
-                <p><strong>Courses:</strong> ${edu.courses.join(', ')}</p>` : ''}
-            ${edu.description ? `<p>${edu.description}</p>` : ''}
         </div>
     `).join('');
     educationSection.innerHTML = educationContainer;
@@ -42,18 +46,18 @@ function populateEducation(education) {
 function populateWorkExperience(workExperience) {
     const workExperienceSection = document.getElementById('experience-body');
     const jobsContainer = workExperience.map(job => `
-        <div>
-            <div class="left-right">
-                <h3> ${job.companyName} </h3>
-                <p><i> ${job.dates} </i></p>
-            </div>
-            <div class="left-right">
-                <h4> ${job.title} </h4>
-                <p><i> ${job.location} </i></p>
-            </div>
-            <nav class="left-navbar">
+        <div class="portfolio-section">
+            <div>
+                <div class="left-right">
+                    <h3> ${job.companyName} </h3>
+                    <p><i> ${job.dates} </i></p>
+                </div>
+                <div class="left-right">
+                    <h4> ${job.title} </h4>
+                    <p><i> ${job.location} </i></p>
+                </div>
                 ${parseAssets(job.assets)}
-            </nav>
+            </div>
             <ul class="bullets">
                 ${job.description.map(desc => `<li>${desc}</li>`).join('')}
             </ul>
@@ -66,14 +70,14 @@ function populateWorkExperience(workExperience) {
 function populateProjects(projects) {
     const projectsSection = document.getElementById('projects-body')
     const educationContainer = projects.map(proj => `
-        <div>
-            <div class="left-right">
-                <h3> ${proj.name} </h3>
-                <p><i> ${proj.date} </i></p>
-            </div>
-            <nav class="left-navbar">
+        <div class="portfolio-section">
+            <div>
+                <div class="left-right">
+                    <h3> ${proj.name} </h3>
+                    <p><i> ${proj.date} </i></p>
+                </div>
                 ${parseAssets(proj.assets)}
-            </nav>
+            </div>
             <ul class="bullets">
                 ${proj.description.map(desc => `<li>${desc}</li>`).join('')}
             </ul>
@@ -83,9 +87,9 @@ function populateProjects(projects) {
 }
 
 function parseAssets(assets) {
-    let html = '';
+    let links = [];
     Object.keys(assets).forEach(key => {
-        html += `<a href="${assets[key]}" target="_blank">${key}</a>`;
+        links.push(`<a href="${assets[key]}" target="_blank">${key}</a>`);
     });
-    return html;
+    return links.join(', ');
 }
