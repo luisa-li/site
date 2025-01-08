097ae8774dff2a032a609e6b24f4d6f9f6cfc900
Luisa <103768169+luisa-li@users.noreply.github.com>
2025-01-08 15:21:31 -0500

    prettier and jukebox fix

--- api/index.py
+++ api/index.py
@@ -1,7 +1,7 @@
 import os
 import base64
 import requests
-from fastapi import FastAPI, Response, status 
+from fastapi import FastAPI, Response, status
 from fastapi.responses import FileResponse
 from dotenv import load_dotenv
 from datetime import datetime, timezone
@@ -23,9 +23,9 @@ SCOPE = "user-read-playback-state"
 app = FastAPI()
 
 origins = [
-    "https://luisali.com", 
-    "https://*.vercel.app",  
-    "http://127.0.0.1:5500/*", 
+    "https://luisali.com",
+    "https://*.vercel.app",
+    "http://127.0.0.1:5500/*",
 ]
 
 app.add_middleware(
@@ -33,17 +33,21 @@ app.add_middleware(
     allow_origins=origins,
     allow_credentials=True,
     allow_methods=["GET", "POST", "PUT", "DELETE"],
-    allow_headers=["Authorization", "Content-Type"], 
+    allow_headers=["Authorization", "Content-Type"],
 )
+
 
 def get_spotify_access_key():
     # Your logic to get the access key
-    key, updated_at = get_access_key('spotify')
-    updated_at_datetime = datetime.fromisoformat(updated_at).replace(tzinfo=timezone.utc)
+    key, updated_at = get_access_key("spotify")
+    updated_at_datetime = datetime.fromisoformat(updated_at).replace(
+        tzinfo=timezone.utc
+    )
     if (datetime.now(timezone.utc) - updated_at_datetime).total_seconds() > 3600:
         key = refresh_key(REFRESH_TOKEN)
         update_access_key("spotify", key)
     return key
+
 
 def refresh_key(refresh_token):
     url = SPOTIFY_TOKEN_URL
@@ -55,26 +59,25 @@ def refresh_key(refresh_token):
         "refresh_token": refresh_token,
     }
     response = requests.post(url, headers=headers, data=data)
-    
+
     if response.status_code == 200:
         tokens = response.json()
         return tokens.get("access_token")
     else:
         return None
 
+
 @app.get("/listening")
 async def get_current_song():
-    headers = {
-        "Authorization": f"Bearer {get_spotify_access_key()}"
-    }
+    headers = {"Authorization": f"Bearer {get_spotify_access_key()}"}
     response = requests.get(SPOTIFY_CURRENTLY_PLAYING_URL, headers=headers)
 
     if response.status_code == 200:
+
         data = response.json()
         track = data.get("item")
-        is_playing = data.get("is_playing", False)
 
-        if track and is_playing:
+        if track:
             track_id = track.get("id")
             embed_url = f"https://open.spotify.com/embed/track/{track_id}"
             html_snippet = f"""
@@ -86,6 +89,7 @@ async def get_current_song():
         return Response(content=None, status_code=status.HTTP_200_OK)
     else:
         return Response(content=None, status_code=status.HTTP_400_BAD_REQUEST)
+
 
 @app.get("/{file_name}")
 async def serve_static_files(file_name: str):
